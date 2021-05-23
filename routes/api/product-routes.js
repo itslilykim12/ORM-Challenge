1ce57e2f8a50e6bb879575700c0e873fcae88cd8
nlilu <nlilum.kim@gmail.com>
2021-05-23 12:55:40 -0400

    updated the api routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,14 +5,33 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+ Product.findAll({
+   include: [
+     Category, {
+       model: Tag,
+       through: ProductTag,
+     },
+   ],
+ }).then(results => {
+   res.json(results);
+ });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+ Product.findAll({
+   where: {
+     id: req.params.id,
+   },
+   include: [
+     Category, {
+       model: Tag,
+       through: ProductTag,
+     },
+   ],
+ }).then(results => {
+   res.json(results);
+ });
 });
 
 // create new product
@@ -25,8 +44,15 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
-    .then((product) => {
+  Product.create({
+    id: req.body.id,
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tag_name: req.body.tag_name,
+  })
+  .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
@@ -50,12 +76,20 @@ router.post('/', (req, res) => {
 // update product
 router.put('/:id', (req, res) => {
   // update product data
-  Product.update(req.body, {
+  Product.update(
+    {
+      id: req.body.id,
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock,
+      category_id: req.body.category_id,
+      tag_name: req.body.tag_name,
+    },
+    {
     where: {
       id: req.params.id,
     },
-  })
-    .then((product) => {
+  }).then((product) => {
       // find all associated tags from ProductTag
       return ProductTag.findAll({ where: { product_id: req.params.id } });
     })
@@ -91,6 +125,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    }
+  }).then(results => {
+    res.json(results);
+  });
 });
 
 module.exports = router;
